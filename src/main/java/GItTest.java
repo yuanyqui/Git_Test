80ba3a68adf085e56793b9680d32e5fd7bc66c37
yyq <yyq@qq.com>
2023-01-16 01:51:14 +0800

    master version

---
+++ src/main/java/GItTest.java
@@ -0,0 +1,8 @@
+import sun.applet.Main;
+
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("master version");
+    }
+}
