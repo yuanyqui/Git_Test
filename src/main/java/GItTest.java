a3cd168a252f85550984851eb5f613d72074bda4
yyq <yyq@qq.com>
2023-01-16 01:56:48 +0800

    master version1

--- src/main/java/GItTest.java
+++ src/main/java/GItTest.java
@@ -4,5 +4,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("master version");
+        System.out.println("master version1");
     }
 }
